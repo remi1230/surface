0b4c8841b7e83f1567afa1bbaa37f7e15b218f20
Rémi Tafforeau <contact@my1prod.com>
2026-03-27 10:13:38 +0100

    Add Liquid shader

--- js/shaders-frags.js
+++ js/shaders-frags.js
@@ -52,6 +52,7 @@
  * 19 - Random Perlin noise
  * 20 - Starfield
  * 21 - Grid uvParams (heatmap)
+ * 22 - Liquid
  *
  * @type {string[]}
  */
@@ -327,6 +328,42 @@ fragmentShaders = [
     col = val1 + val2;
 
 `,
+`
+    //Liquid
+    float PI = 3.14159;
+    vec2 uvL = vec2(.125,.75)+(vUVParams*uvParamsCoeff-vec2(.125,.75))*.03;
+    float T=time*.25;
+
+    vec3 c = clamp(1.-.7*vec3(
+        length(uvL-vec2(.1,0)),
+        length(uvL-vec2(.9,0)),
+        length(uvL-vec2(.5,1))
+        ),0.,1.)*2.-1.;
+
+    vec3 c0=vec3(0);
+    float w0=0.;
+    const float N=16.;
+    for(float i=0.;i<N;i++)
+    {
+        float wt=(i*i/N/N-.2)*.3;
+        float wp=0.5+(i+1.)*(i+1.5)*0.001;
+        float wb=.05+i/N*0.1;
+    	c.zx=rotL(c.zx,1.6+T*0.65*wt+(uvL.x+.7)*23.*wp);
+    	c.xy=rotL(c.xy,c.z*c.x*wb+1.7+T*wt+(uvL.y+1.1)*15.*wp);
+    	c.yz=rotL(c.yz,c.x*c.y*wb+2.4-T*0.79*wt+(uvL.x+uvL.y*(fract(i/2.)-0.25)*4.)*17.*wp);
+    	c.zx=rotL(c.zx,c.y*c.z*wb+1.6-T*0.65*wt+(uvL.x+.7)*23.*wp);
+    	c.xy=rotL(c.xy,c.z*c.x*wb+1.7-T*wt+(uvL.y+1.1)*15.*wp);
+        float w=(1.5-i/N);
+        c0+=c*w;
+        w0+=w;
+    }
+    c0=c0/w0*2.+.5;//*(1.-pow(uvL.y-.5,2.)*2.)*2.+.5;
+    c0*=.5+dot(c0,vec3(1,1,1))/sqrt(3.)*.5;
+    c0+=pow(length(sin(c0*PI*4.))/sqrt(3.)*1.0,20.)*(.3+.7*c0);
+
+	col = c0;
+
+`,
 
 ];
 
@@ -507,6 +544,13 @@ vec2 rotate2D (vec2 _st, float _angle) {
                 sin(_angle),cos(_angle)) * _st;
     _st += 0.5;
     return _st;
+}
+
+vec2 rotL(vec2 p,float a)
+{
+    float c=cos(a*15.83);
+    float s=sin(a*15.83);
+    return p*mat2(s,c,c,-s);
 }
 
 mat2 rot(float _angle){
